7437308ba73eb48a154b88bafdd2bf4a3664ef78
Eduardo Ortega <195473867+EduOrt64@users.noreply.github.com>
2026-03-19 16:49:42 -0600

    finished adding react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
+import { Routes, Route } from 'react-router-dom'
 import HomeHero from './components/homehrero'
 import Navbar from './components/navbar'
 
 function App() {
-
   return (
     <>
-    <Navbar isLogged={false} />
-    <HomeHero />
+      <Navbar isLogged={false} />
+
+      <Routes>
+        <Route path="/" element={<HomeHero />} />
+      </Routes>
     </>
   )
 }
